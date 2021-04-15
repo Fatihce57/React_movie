6186c8b84b98c01b7251426e30e38f5abec4b226
Fatih Cengiz <fatihce57@gmail.com>
2021-04-16 00:27:41 +0200

    searchbar done

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 class SearchBar extends React.Component {
 
 state = {
-    searchQuery:""
+  
 }
 
 handleFormSubmit = (event) => {
@@ -17,10 +17,11 @@ return (
 <form onSubmit={this.handleFormSubmit}>
     <div className="form-row mb-5">
         <div className="col-12">
-            <input onChange={(event) => this.setState({searchQuery:event.target.value})} 
+            <input 
+            onChange={this.props.searchMovieProp} 
             type="text" className="form-control" 
             placeholder="Search a movie"
-            value={this.state.searchQuery} />
+             />
         </div>
     </div>
 </form>
